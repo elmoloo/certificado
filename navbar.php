62ec63537fb7c36cedd161b8f61ca8e3e10e83d8
elmoloo <duranstalin10@gmail.com>
2023-10-27 19:47:12 -0500

    editando referencias de url

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <a class="navbar-brand" href="#">
-   <img src="/mariano_certificado/assets/images/itsms.png"  width="35" height="35" alt="">
+   <img src="./assets/images/itsms.png"  width="35" height="35" alt="">
    </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -10,16 +10,16 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="/mariano_certificado/index.php"><i class="fas fa-home"></i> Inicio <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="./index.php"><i class="fas fa-home"></i> Inicio <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <i class="fas fa-clipboard"></i> Malla curricular
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="/mariano_certificado/add/add-especialidad.php">Crear Especialidad</a>
-          <a class="dropdown-item" href="/mariano_certificado/add/add-periodo-academico.php">Crear Periodo Academico</a>
-          <a class="dropdown-item" href="/mariano_certificado/curso.php">Curso</a>
+          <a class="dropdown-item" href="./add/add-especialidad.php">Crear Especialidad</a>
+          <a class="dropdown-item" href="./add/add-periodo-academico.php">Crear Periodo Academico</a>
+          <a class="dropdown-item" href="./curso.php">Curso</a>
         </div> 
      
       </li>
@@ -28,8 +28,8 @@
         <i class="fas fa-user-graduate"></i> Estudiante
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="/mariano_certificado/estudiante.php">Estudiante</a>
-          <a class="dropdown-item" href="/mariano_certificado/add/add-listado-estudiantes.php">Asistencia</a>
+          <a class="dropdown-item" href="./estudiante.php">Estudiante</a>
+          <a class="dropdown-item" href="./add/add-listado-estudiantes.php">Asistencia</a>
         </div> 
       </li>
       <li class="nav-item dropdown">
@@ -37,9 +37,9 @@
         <i class="fas fa-book-open"></i> Ficheros
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="/mariano_certificado/add/add-coordinador.php">Añadir Coordinador</a>
-          <a class="dropdown-item" href="/mariano_certificado/add/add-facilitador.php">Añadir Facilitador</a>
-          <a class="dropdown-item" href="/mariano_certificado/add/add-rector.php">Añadir Rector</a>
+          <a class="dropdown-item" href="./add/add-coordinador.php">Añadir Coordinador</a>
+          <a class="dropdown-item" href="./add/add-facilitador.php">Añadir Facilitador</a>
+          <a class="dropdown-item" href="./add/add-rector.php">Añadir Rector</a>
           
 
         </div> 
@@ -60,10 +60,10 @@
         <i class="fas fa-map-marked-alt"></i> Ubicacion
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="/mariano_certificado/add/add-pais.php">Añadir nuevo Pais</a>
-          <a class="dropdown-item" href="/mariano_certificado/add/add-provincia.php">Añadir nueva Provincia</a>
-           <a class="dropdown-item" href="/mariano_certificado/add/add-canton.php">Añadir nuevo Canton</a>
-           <a class="dropdown-item" href="/mariano_certificado/add/add-ciudad.php">Añadir nueva Ciudad</a>
+          <a class="dropdown-item" href="./add/add-pais.php">Añadir nuevo Pais</a>
+          <a class="dropdown-item" href="./add/add-provincia.php">Añadir nueva Provincia</a>
+           <a class="dropdown-item" href="./add/add-canton.php">Añadir nuevo Canton</a>
+           <a class="dropdown-item" href="./add/add-ciudad.php">Añadir nueva Ciudad</a>
         </div>
       </li>
     </ul>
